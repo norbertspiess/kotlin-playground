ca5bba59b3208e77e1fbefcca760f818cd6f977e
Norbert Spieß <norbertspiess@users.noreply.github.com>
2017-09-30 20:13:45 +0200

    typical flow statements

---
+++ src/de/norbert/kotlin/playground/Flow.kt
@@ -0,0 +1,61 @@
+package de.norbert.kotlin.playground
+
+class Flow {
+
+    fun ifStatement(a: Int, b: Int) {
+        val max = if (a > b) a else b
+        println("max: $max")
+    }
+
+    fun whenStatement(input: Any) {
+        val type = when (input) {
+            is String -> "string"
+            is Int -> "int"
+            else -> "i don't know what"
+        }
+        val strInput = input.toString()
+        val result = when (strInput) {
+            "a" -> "you gave me an a"
+            in "b", "c", "d" -> "either b, c or d"
+            else -> "you gave me something unexpected"
+        }
+        println("input is of type $type and was $result")
+    }
+
+    fun whileLoop() {
+        var ctr = 6
+        while (ctr > 0) {
+            println("counting down: $ctr")
+            ctr--
+        }
+    }
+
+    fun forLoop() {
+        for (i in 1..10) {
+            println("counting up: $i")
+        }
+        println()
+        for (i in 10 downTo 1) {
+            println("counting down: $i")
+        }
+        println()
+        for (i in 10 downTo 1 step 2) {
+            println("counting down with steps: $i")
+        }
+        println()
+        val language = arrayOf("Ruby", "Kotlin", "Python", "Java")
+        for (item in language)
+            println(item)
+
+        println()
+        for (item in language.indices) {
+            // printing array elements having even index only
+            if (item % 2 == 0)
+                println(language[item])
+        }
+        println()
+        for (letter in "lorem ipsum") {
+            print(letter)
+        }
+    }
+}
--- src/de/norbert/kotlin/playground/Main.kt
+++ src/de/norbert/kotlin/playground/Main.kt
@@ -1,5 +1,9 @@
 package de.norbert.kotlin.playground
 
 fun main(args: Array<String>) {
-    println("Hello World")
+    val flow = Flow()
+    flow.ifStatement(5, 6)
+    flow.whenStatement("b")
+    flow.forLoop()
+    flow.whenStatement("lorem")
 }
